6f5a5ee1d984e3bda233b27fb6ad0981499aa460
Jacob Boyce <jboyce313@gmail.com>
2023-01-15 17:33:59 -0600

    edited comments in script.js

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ $(function () {
   var container = $(".container-fluid");
   var currentDay = $("#currentDay");
 
+  // set current day display text
   currentDay.text(today.$d);
 
+  // apply past, present, or future classes to time blocks
   container.children().each(function () {
     var hourString = this.id.match(/\d+/);
     var hour = parseInt(hourString);
@@ -23,20 +25,13 @@ $(function () {
     }
   });
 
-  // TODO: Add a listener for click events on the save button. This code should
+  // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
   saveBtn.on("click", function () {
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
     var hourBlock = $(this).parent();
-
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
   });
 
   // TODO: Add code to get any user input that was saved in localStorage and set
