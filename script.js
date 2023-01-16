2e772ac76c3323f859806863183cbb95e2b969b2
Jacob Boyce <jboyce313@gmail.com>
2023-01-16 09:16:28 -0600

    added functionality to display saved events

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   var today = dayjs();
   var currentHour = today.$H;
@@ -24,19 +21,20 @@ $(function () {
     }
   });
 
-  // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // display saved events in hour blocks
+  for (var i = 9; i < 18; i++) {
+    var hr = `hour-${i}`;
+    if (localStorage.getItem(hr)) {
+      var event = localStorage.getItem(hr);
+      $(`#${hr}`).children("textarea").text(event);
+    }
+  }
+
+  // save events in local storage
   saveBtn.on("click", function () {
     var hourBlock = $(this).parent();
     var hourID = hourBlock.attr("id");
     var event = hourBlock.children("textarea").val();
     localStorage.setItem(hourID, event);
   });
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
 });
